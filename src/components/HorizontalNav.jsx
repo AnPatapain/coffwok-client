1f4de3bb327ffa636d1f3c1dd3cc0c8795fde292
anpatapain <nkalk192002@gmail.com>
2023-07-09 10:48:48 +0200

    deploy: change h2 for horizontal nav

--- src/components/HorizontalNav.jsx
+++ src/components/HorizontalNav.jsx
@@ -3,7 +3,7 @@ import {GrMapLocation} from "react-icons/gr";
 const HorizontalNav = () => {
     return (
         <div className="horizontal-nav">
-            <h2>Bảng kế hoạch</h2>
+            <h2>Kế hoạch cà phê - học bài</h2>
             <span>
                 <GrMapLocation className="location-icon"/>
                 <h4>Quy Nhơn</h4>
